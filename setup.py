e5fdcb9ea81cd24704535589ec60207142481bd2
Hanno Schlichting <hanno@hannosch.eu>
2009-06-05 14:34:34 +0000

    Added undeclared dependency on Products.PloneLanguageTool >= 3.0.
    svn path=/Products.LinguaPlone/trunk/; revision=27321

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(name='Products.LinguaPlone',
       install_requires=[
         'setuptools',
         'Plone >= 3.2'
+        'Products.PloneLanguageTool >= 3.0',
         'plone.browserlayer',
         'plone.app.layout >= 1.1.4',
       ],
