8ad17475dfc470052c4ff5eb60d5918d32e3586a
Hanno Schlichting <hanno@hannosch.eu>
2009-09-26 11:49:03 +0000

    Refactored common functionality of the catalog exportimport handlers. Added automatic reindexing for newly added indexes.
    svn path=/Products.LinguaPlone/trunk/; revision=29902

--- Products/LinguaPlone/exportimport/reference.py
+++ Products/LinguaPlone/exportimport/reference.py
@@ -1,57 +1,30 @@
 from zope.component import adapts
 
 from Products.Archetypes.interfaces import IReferenceCatalog
-from Products.CMFCore.utils import getToolByName
 from Products.GenericSetup.interfaces import ISetupEnviron
-from Products.GenericSetup.utils import exportObjects
-from Products.GenericSetup.utils import importObjects
-from Products.GenericSetup.ZCatalog.exportimport import ZCatalogXMLAdapter
+
+from Products.LinguaPlone.exportimport import catalog
+
+NAME = 'reference_catalog'
 
 
 def importCatalogTool(context):
-    """Import reference catalog.
+    """Import catalog.
     """
-    site = context.getSite()
-    tool = getToolByName(site, 'reference_catalog', None)
-    if tool is not None:
-        importObjects(tool, '', context)
+    catalog.importCatalogTool(context, name=NAME)
 
 
 def exportCatalogTool(context):
-    """Export reference catalog.
+    """Export catalog.
     """
-    site = context.getSite()
-    tool = getToolByName(site, 'reference_catalog', None)
-    if tool is None:
-        logger = context.getLogger('catalog')
-        logger.info('Nothing to export.')
-        return
-
-    exportObjects(tool, '', context)
+    catalog.exportCatalogTool(context, name=NAME)
 
 
-class ReferenceCatalogXMLAdapter(ZCatalogXMLAdapter):
-    """XML im- and exporter for the Reference Catalog.
+class ReferenceCatalogXMLAdapter(catalog.CatalogXMLAdapter):
+    """XML im- and exporter for the reference catalog.
     """
 
     adapts(IReferenceCatalog, ISetupEnviron)
 
-    _LOGGER_ID = 'reference_catalog'
-
-    name = 'reference_catalog'
-
-    def _initColumns(self, node):
-        for child in node.childNodes:
-            if child.nodeName != 'column':
-                continue
-            col = str(child.getAttribute('value'))
-            if child.hasAttribute('remove'):
-                # Remove the column if it is there
-                if col in self.context.schema()[:]:
-                    self.context.delColumn(col)
-                continue
-            if col not in self.context.schema()[:]:
-                self.context.addColumn(col)
-                # If we added a new column we need to update the
-                # metadata even if this will take a while
-                self.context.refreshCatalog()
+    _LOGGER_ID = NAME
+    name = NAME
--- Products/LinguaPlone/exportimport/uid.py
+++ Products/LinguaPlone/exportimport/uid.py
@@ -1,57 +1,30 @@
 from zope.component import adapts
 
 from Products.Archetypes.interfaces import IUIDCatalog
-from Products.CMFCore.utils import getToolByName
 from Products.GenericSetup.interfaces import ISetupEnviron
-from Products.GenericSetup.utils import exportObjects
-from Products.GenericSetup.utils import importObjects
-from Products.GenericSetup.ZCatalog.exportimport import ZCatalogXMLAdapter
+
+from Products.LinguaPlone.exportimport import catalog
+
+NAME = 'uid_catalog'
 
 
 def importCatalogTool(context):
-    """Import uid catalog.
+    """Import catalog.
     """
-    site = context.getSite()
-    tool = getToolByName(site, 'uid_catalog', None)
-    if tool is not None:
-        importObjects(tool, '', context)
+    catalog.importCatalogTool(context, name=NAME)
 
 
 def exportCatalogTool(context):
-    """Export uid catalog.
+    """Export catalog.
     """
-    site = context.getSite()
-    tool = getToolByName(site, 'uid_catalog', None)
-    if tool is None:
-        logger = context.getLogger('catalog')
-        logger.info('Nothing to export.')
-        return
-
-    exportObjects(tool, '', context)
+    catalog.exportCatalogTool(context, name=NAME)
 
 
-class UIDCatalogXMLAdapter(ZCatalogXMLAdapter):
-    """XML im- and exporter for the UID Catalog.
+class UIDCatalogXMLAdapter(catalog.CatalogXMLAdapter):
+    """XML im- and exporter for the UID catalog.
     """
 
     adapts(IUIDCatalog, ISetupEnviron)
 
-    _LOGGER_ID = 'uid_catalog'
-
-    name = 'uid_catalog'
-
-    def _initColumns(self, node):
-        for child in node.childNodes:
-            if child.nodeName != 'column':
-                continue
-            col = str(child.getAttribute('value'))
-            if child.hasAttribute('remove'):
-                # Remove the column if it is there
-                if col in self.context.schema()[:]:
-                    self.context.delColumn(col)
-                continue
-            if col not in self.context.schema()[:]:
-                self.context.addColumn(col)
-                # If we added a new column we need to update the
-                # metadata even if this will take a while
-                self.context.refreshCatalog()
+    _LOGGER_ID = NAME
+    name = NAME
