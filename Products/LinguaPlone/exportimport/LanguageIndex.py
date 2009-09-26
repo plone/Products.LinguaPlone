1b75ba931a1fc44ccab90e1ab0cf30f506ea520b
Hanno Schlichting <hanno@hannosch.eu>
2009-09-26 11:57:00 +0000

    Modernized the code of the language index export import handler.
    svn path=/Products.LinguaPlone/trunk/; revision=29903

--- Products/LinguaPlone/exportimport/LanguageIndex.py
+++ Products/LinguaPlone/exportimport/LanguageIndex.py
@@ -1,31 +1,17 @@
-# Plone Solutions AS <info@plonesolutions.com>
-# Martijn Pieters <mj@plonesolutions.com>
-# http://www.plonesolutions.com
+from zope.component import adapts
 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-from Products.GenericSetup.utils import NodeAdapterBase, PropertyManagerHelpers
+from Products.GenericSetup.interfaces import ISetupEnviron
+from Products.GenericSetup.utils import NodeAdapterBase
+from Products.GenericSetup.utils import PropertyManagerHelpers
 
 from Products.LinguaPlone.interfaces import ILanguageIndex
 
-class LanguageIndexNodeAdapter(NodeAdapterBase, PropertyManagerHelpers):
 
+class LanguageIndexNodeAdapter(NodeAdapterBase, PropertyManagerHelpers):
     """Node im- and exporter for LanguageIndex.
     """
 
-    __used_for__ = ILanguageIndex
+    adapts(ILanguageIndex, ISetupEnviron)
 
     def _exportNode(self):
         """Export the object as a DOM node."""
