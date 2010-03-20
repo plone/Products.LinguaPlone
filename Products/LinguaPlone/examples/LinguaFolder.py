f188609ce810464930785a511abcadd4cadc8ed8
Hanno Schlichting <hanno@hannosch.eu>
2010-03-20 21:49:41 +0000

    Reformat examples
    svn path=/Products.LinguaPlone/trunk/; revision=35421

--- Products/LinguaPlone/examples/LinguaFolder.py
+++ Products/LinguaPlone/examples/LinguaFolder.py
@@ -2,19 +2,15 @@ from AccessControl import ClassSecurityInfo
 
 try:
     from Products.LinguaPlone.public import *
-except ImportError:
-    # No multilingual support
+except ImportError: # pragma: no cover
     from Products.Archetypes.public import *
-
-
-schema = BaseSchema
 
 
 class LinguaFolder(BaseFolder):
     """A simple folderish multilingual archetype"""
-    archetypes_name = portal_type = meta_type = 'Lingua Folder'
-    schema = schema
 
+    archetypes_name = portal_type = meta_type = 'Lingua Folder'
+    schema = BaseSchema
     security = ClassSecurityInfo()
 
 
--- Products/LinguaPlone/examples/LinguaItem.py
+++ Products/LinguaPlone/examples/LinguaItem.py
@@ -1,72 +1,52 @@
 from AccessControl import ClassSecurityInfo
-from Products.CMFCore.permissions import ModifyPortalContent
 
 try:
     from Products.LinguaPlone.public import *
-except ImportError:
-    # No multilingual support
+except ImportError: # pragma: no cover
     from Products.Archetypes.public import *
 
+
 schema = BaseSchema + Schema((
-    StringField('contact',
-                languageIndependent = 1,
-                widget = StringWidget(
-                    description="Enter a contact email address (this field is "
-                                "language independent)."),
-               ),
-    TextField('body',
-              required = 1,
-              searchable = 1,
-              default_output_type = 'text/html',
-              allowable_content_types = ('text/plain',
-                                         'text/restructured',
-                                         'text/html',
-                                         'application/msword'),
-              widget = RichWidget(description="Enter or upload text for the"
-                                              " body of the document."),
-             ),
+
+    StringField(
+        name = 'contact',
+        languageIndependent = True,
+        widget = StringWidget(
+            description="Enter a contact email address).")),
+
+    TextField(
+        name = 'body',
+        required = True,
+        searchable = True,
+        default_output_type = 'text/html',
+        allowable_content_types = ('text/plain',
+                                  'text/restructured',
+                                  'text/html',
+                                  'application/msword'),
+        widget = RichWidget(
+            description="Enter or upload text for the body of the document.")),
 
     ImageField(
-        name='image',
-        languageIndependent=True,
-        widget=ImageWidget(
-            description='Image Independent',
-        ),
-    ),
+        name = 'image',
+        languageIndependent = True,
+        widget = ImageWidget(
+            description='Image Independent')),
 
     ImageField(
-        name='imageDependent',
-        languageIndependent=False,
-        widget=ImageWidget(
-            description='Image Dependent',
-        ),
-    ),
+        name = 'imageDependent',
+        languageIndependent = False,
+        widget = ImageWidget(
+            description='Image Dependent')),
 
 ))
 
 
 class LinguaItem(BaseContent):
     """A simple multilingual archetype"""
+
     archetypes_name = portal_type = meta_type = 'Lingua Item'
     schema = schema
-
-    actions = [
-        {'id': 'edit',
-         'name': 'Edit',
-         'action': 'string:${object_url}/base_edit',
-         'condition':
-            'python: object.isTranslatable() and object.isCanonical()',
-         'permissions': (ModifyPortalContent, ),
-        },
-       {'id': 'translate',
-        'name': 'Edit',
-        'action': 'string:${object_url}/translate_item',
-        'condition':
-            'python:object.isTranslatable() and not object.isCanonical()',
-        'permissions': (ModifyPortalContent, ),
-        },
-    ]
-
     security = ClassSecurityInfo()
+
 
 registerType(LinguaItem, 'LinguaPlone')
