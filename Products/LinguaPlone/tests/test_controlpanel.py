4902e5092a28ab78a4aeb2a61426dcd7314831e8
Hanno Schlichting <hanno@hannosch.eu>
2010-03-21 01:24:41 +0000

    Expanded tests for the language control panel
    svn path=/Products.LinguaPlone/trunk/; revision=35437

--- Products/LinguaPlone/tests/test_controlpanel.py
+++ Products/LinguaPlone/tests/test_controlpanel.py
@@ -1,7 +1,9 @@
 from zope.interface import directlyProvides
 
+from Products.CMFCore.utils import getToolByName
 from Testing.makerequest import makerequest
 
+from Products.LinguaPlone.browser import controlpanel
 from Products.LinguaPlone.interfaces import ILinguaPloneProductLayer
 from Products.LinguaPlone.tests import LinguaPloneTestCase
 
@@ -12,6 +14,7 @@ class TestLanguageControlPanel(LinguaPloneTestCase.LinguaPloneTestCase):
         self.addLanguage('de')
         self.addLanguage('no')
         self.setLanguage('en')
+        self.langs = set(('de', 'en', 'no'))
 
     def testRenderControlPanel(self):
         self.loginAsPortalOwner()
@@ -32,6 +35,14 @@ class TestLanguageControlPanel(LinguaPloneTestCase.LinguaPloneTestCase):
         self.assert_("Language Settings" in output)
         self.assert_("Site language" in output)
         self.assert_("Available languages" not in output)
+
+    def testAvailableLanguages(self):
+        adapter = controlpanel.IMultiLanguageSelectionSchema(self.portal)
+        self.assertEquals(set(adapter.available_languages), self.langs)
+        new = ['en', 'de']
+        adapter.available_languages = new
+        ltool = getToolByName(self.portal, 'portal_languages')
+        self.assertEquals(set(ltool.getSupportedLanguages()), set(new))
 
 
 def test_suite():
