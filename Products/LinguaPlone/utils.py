c5e8ef157dde6f55e76d9878deb32ffd4079aed3
Hanno Schlichting <hanno@hannosch.eu>
2010-04-19 14:37:01 +0000

    Shorten isinstance condition
    svn path=/Products.LinguaPlone/trunk/; revision=36222

--- Products/LinguaPlone/utils.py
+++ Products/LinguaPlone/utils.py
@@ -40,7 +40,7 @@ def translated_references(context, language, sources):
     should those exist."""
     if not sources:
         return sources
-    if not isinstance(sources, list) and not isinstance(sources, tuple):
+    if not isinstance(sources, (list, tuple)):
         sources = [sources]
 
     result = []
