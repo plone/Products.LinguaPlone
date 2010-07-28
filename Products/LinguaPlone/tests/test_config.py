440ca4c42111eb917a926b0d748a597f013a13f3
Hanno Schlichting <hanno@hannosch.eu>
2010-07-28 23:09:20 +0000

    Add full test coverage for config module
    svn path=/Products.LinguaPlone/trunk/; revision=38324

---
+++ Products/LinguaPlone/tests/test_config.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+
+import os
+from unittest import TestCase
+
+
+class TestConfig(TestCase):
+
+    def test_relationship_key(self):
+        from ..config import RELATIONSHIP
+        self.assertEquals(RELATIONSHIP, 'translationOf')
+
+    def test_i18naware_catalog(self):
+        from ..config import I18NAWARE_CATALOG
+        self.assertEquals(I18NAWARE_CATALOG, True)
+
+    def test_i18naware_catalog_set(self):
+        from Products.LinguaPlone import config
+        envkey = 'PLONE_I18NAWARE_CATALOG'
+        self.assertEquals(config.I18NAWARE_CATALOG, True)
+        try:
+            os.environ[envkey] = 'false'
+            reload(config)
+            self.assertEquals(config.I18NAWARE_CATALOG, False)
+        finally:
+            del os.environ[envkey]
+            reload(config)
+
+    def test_nofilterkeys(self):
+        from ..config import NOFILTERKEYS
+        expected = set(['Language', 'UID', 'id', 'getId'])
+        self.assertEquals(set(NOFILTERKEYS), expected)
+
+    def test_reference_fields(self):
+        from ..config import I18NAWARE_REFERENCE_FIELDS
+        from Products.Archetypes.Field import ReferenceField
+        self.assert_(ReferenceField in I18NAWARE_REFERENCE_FIELDS)
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestConfig))
+    return suite
