8627fcc6719ed5d4f537bf276e98a84ff8cfe274
Hanno Schlichting <hanno@hannosch.eu>
2010-07-28 23:30:15 +0000

    Test all of setuphandlers
    svn path=/Products.LinguaPlone/trunk/; revision=38325

--- Products/LinguaPlone/tests/test_setup.py
+++ Products/LinguaPlone/tests/test_setup.py
@@ -1,38 +1,80 @@
-from Acquisition import aq_base
-from Products.CMFPlone.CatalogTool import CatalogTool
+import logging
 
-from Products.LinguaPlone import config
+from Acquisition import aq_base
+from Products.CMFCore.utils import getToolByName
+
 from Products.LinguaPlone.tests.base import LinguaPloneTestCase
 
 
-class TestPatchesSetup(LinguaPloneTestCase):
+class Extra(object):
 
-    def testCatalogPatch(self):
+    fallback = False
+
+
+class Context(object):
+
+    def __init__(self, site):
+        self.site = site
+
+    def readDataFile(self, name):
+        return True
+
+    def getSite(self):
+        return self.site
+
+    def getLogger(self, name):
+        return logging.getLogger(name)
+
+
+class TestInstallSetup(LinguaPloneTestCase):
+
+    def test_catalog_patch(self):
+        from Products.CMFPlone.CatalogTool import CatalogTool
+        from Products.LinguaPlone import config
         if config.I18NAWARE_CATALOG:
             self.failUnless(hasattr(CatalogTool, '__lp_old_searchResults'))
         else:
             self.failIf(hasattr(CatalogTool, '__lp_old_searchResults'))
 
-
-class TestInstallSetup(LinguaPloneTestCase):
-
-    def testTools(self):
-        # Check all tools are installed
+    def test_tools(self):
         portal = aq_base(self.portal)
         self.failUnless(hasattr(portal, 'archetype_tool'))
         self.failUnless(hasattr(portal, 'portal_languages'))
 
-    def testPortalTypes(self):
-        # Check all content types are installed
-        types = aq_base(self.portal.portal_types)
+    def test_portal_types(self):
+        types = aq_base(getToolByName(self.portal, 'portal_types'))
         self.failUnless(hasattr(types, 'SimpleType'))
         self.failUnless(hasattr(types, 'DerivedType'))
         self.failUnless(hasattr(types, 'SimpleFolder'))
+
+    def test_language_index(self):
+        catalog = getToolByName(self.portal, 'portal_catalog')
+        self.assert_('Language' in catalog.indexes())
+        index = catalog._catalog.getIndex('Language')
+        self.assertEquals(index.meta_type, 'FieldIndex')
+        self.assert_(index.numObjects() > 0)
+
+    def test_language_index_replacement(self):
+        from ..setuphandlers import importReindexLanguageIndex
+        catalog = getToolByName(self.portal, 'portal_catalog')
+        self.assert_('Language' in catalog.indexes())
+        catalog.delIndex('Language')
+        catalog.addIndex('Language', 'LanguageIndex', Extra)
+        self.assert_('Language' in catalog.indexes())
+        index = catalog._catalog.getIndex('Language')
+        self.assertEquals(index.meta_type, 'LanguageIndex')
+
+        context = Context(self.portal)
+        importReindexLanguageIndex(context)
+
+        self.assert_('Language' in catalog.indexes())
+        index = catalog._catalog.getIndex('Language')
+        self.assertEquals(index.meta_type, 'FieldIndex')
+        self.assert_(index.numObjects() > 0)
 
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
-    suite.addTest(makeSuite(TestPatchesSetup))
     suite.addTest(makeSuite(TestInstallSetup))
     return suite
