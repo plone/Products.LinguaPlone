0337e04849a14e7d139e79fe64e0f446efaeee88
Hanno Schlichting <hanno@hannosch.eu>
2010-07-28 21:52:40 +0000

    More upgrade tests
    svn path=/Products.LinguaPlone/trunk/; revision=38310

--- Products/LinguaPlone/tests/test_upgrades.py
+++ Products/LinguaPlone/tests/test_upgrades.py
@@ -1,3 +1,5 @@
+from zope.component import getSiteManager
+
 from Products.CMFCore.utils import getToolByName
 
 from Products.LinguaPlone.tests.base import LinguaPloneTestCase
@@ -9,6 +11,16 @@ class TestUpgrades(LinguaPloneTestCase):
         self.loginAsPortalOwner()
         self.addLanguage('de')
         self.addLanguage('no')
+        self.gs = getToolByName(self.portal, 'portal_setup')
+
+    def test_remove_old_import_step(self):
+        from ..migrations import remove_old_import_step
+        registry = self.gs.getImportStepRegistry()
+        old_step = u'linguaplone_various'
+        # TODO add step back
+        for i in range(2):
+            remove_old_import_step(self.gs)
+        self.assert_(old_step not in registry.listSteps())
 
     def test_add_language_metadata(self):
         from ..migrations import add_language_metadata
@@ -16,7 +28,7 @@ class TestUpgrades(LinguaPloneTestCase):
         tool.delColumn('Language')
         self.assert_('Language' not in tool.schema())
         for i in range(2):
-            add_language_metadata(self.portal)
+            add_language_metadata(self.gs)
         self.assert_('Language' in tool.schema())
 
     def test_add_uid_language_index(self):
@@ -25,10 +37,36 @@ class TestUpgrades(LinguaPloneTestCase):
         tool.delIndex('Language')
         self.assert_('Language' not in tool.indexes())
         for i in range(2):
-            add_uid_language_index(self.portal)
+            add_uid_language_index(self.gs)
         self.assert_('Language' in tool.indexes())
         index = tool._catalog.getIndex('Language')
         self.assert_(index.numObjects() > 0)
+
+    def test_add_synced_vocabularies(self):
+        from ..migrations import add_synced_vocabularies
+        from plone.i18n.locales.interfaces import IContentLanguageAvailability
+        from plone.i18n.locales.interfaces import IMetadataLanguageAvailability
+        from Products.LinguaPlone.vocabulary import SyncedLanguages
+        site = getSiteManager(context=self.gs)
+        # TODO register different type of utilities
+        for i in range(2):
+            add_synced_vocabularies(self.gs)
+        util1 = site.queryUtility(IContentLanguageAvailability)
+        self.assertEquals(type(util1), SyncedLanguages)
+        util2 = site.queryUtility(IMetadataLanguageAvailability)
+        self.assertEquals(type(util2), SyncedLanguages)
+
+    def test_add_properties_sheet(self):
+        from ..migrations import add_properties_sheet
+        tool = getToolByName(self.portal, 'portal_properties')
+        del tool['linguaplone_properties']
+        self.assert_('linguaplone_properties' not in tool)
+        for i in range(2):
+            add_properties_sheet(self.gs)
+        self.assert_('linguaplone_properties' in tool)
+        props = tool.linguaplone_properties
+        key = 'hide_right_column_on_translate_form'
+        self.assertEquals(props.getProperty(key), True)
 
 
 def test_suite():
