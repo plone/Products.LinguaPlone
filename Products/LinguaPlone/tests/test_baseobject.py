plone/Products.LinguaPlone

commit eb6143234b29c53b2fa9f1978fe02d2b9b46ac96

fix regression triggered when accessing zope root in ZMI

Products/LinguaPlone/I18NBaseObject.py
@@ -546,7 +546,11 @@ class I18NBaseObject(Implicit):
             return []
 
         site = getSite()
-        tool = getToolByName(site, REFERENCE_CATALOG)
+        if site:
+            tool = getToolByName(site, REFERENCE_CATALOG)
+        else:
+            tool = getToolByName(self, REFERENCE_CATALOG)
+
         _catalog = tool._catalog
         indexes = _catalog.indexes
 

commit 3d8c569e20c61be372daf3f6622a3008a335be45

defer isCanonical computation for getTypeInfo
eb6143234b29c53b2fa9f1978fe02d2b9b46ac96 introduced a patch to fix broken
manage_menu (left pane when using manage).
That fix did not smell good. It was needed because getTypeInfo was computing
isCanonical even though its result is needed only to compute "edit" url.
By deferring the computation of isCanonical to when it is actually needed, the
bad smell can go away.

Products/LinguaPlone/I18NBaseObject.py
@@ -46,8 +46,9 @@ class TypeInfoWrapper:
     used to intercept the edit alias and display translation form"""
     security = ClassSecurityInfo()
 
-    def __init__(self, typeinfo):
+    def __init__(self, typeinfo, contentitem):
         self.__typeinfo = typeinfo
+        self.__contentitem = contentitem
 
     def __nonzero__(self):
         return bool(self.__typeinfo)
@@ -59,7 +60,8 @@ class TypeInfoWrapper:
                 check_visibility, check_condition)
         if action_chain=='object/edit':
             urlparts=res['url'].split('/')
-            if urlparts[-1] in [('atct_edit', 'base_edit')]:
+            if (urlparts[-1] in [('atct_edit', 'base_edit')]
+                    and not self.__contentitem.isCanonical()):
                 urlparts[-1]='translate_item'
                 res['url']='/'.join(urlparts)
         return res
@@ -68,7 +70,8 @@ class TypeInfoWrapper:
     def queryMethodID(self, alias, default=None, context=None):
         if alias == 'edit':
             res = self.__typeinfo.queryMethodID(alias, default, context)
-            if res in ('atct_edit', 'base_edit'):
+            if (res in ('atct_edit', 'base_edit')
+                    and not self.__contentitem.isCanonical()):
                 return 'translate_item'
         return self.__typeinfo.queryMethodID(alias, default, context)
 
@@ -512,8 +515,8 @@ class I18NBaseObject(Implicit):
         possibly wrapped to intercept the edit alias.
         """
         ti = DynamicType.getTypeInfo(self)
-        if ti is not None and not self.isCanonical():
-            return TypeInfoWrapper(ti)
+        if ti is not None:
+            return TypeInfoWrapper(ti, self)
         return ti
 
     security.declareProtected(permissions.View, 'getTranslationReferences')
@@ -546,10 +549,7 @@ class I18NBaseObject(Implicit):
             return []
 
         site = getSite()
-        if site:
-            tool = getToolByName(site, REFERENCE_CATALOG)
-        else:
-            tool = getToolByName(self, REFERENCE_CATALOG)
+        tool = getToolByName(site, REFERENCE_CATALOG)
 
         _catalog = tool._catalog
         indexes = _catalog.indexes

Products/LinguaPlone/tests/test_baseobject.py
@@ -1,0 +1,40 @@
+from Products.LinguaPlone.I18NBaseObject import TypeInfoWrapper
+from Products.LinguaPlone.tests.base import LinguaPloneTestCase
+from Products.LinguaPlone.tests.utils import makeContent
+
+
+class TestBaseObject(LinguaPloneTestCase):
+
+    def afterSetUp(self):
+        self.addLanguage('de')
+        self.addLanguage('fr')
+        self.setLanguage('en')
+        self.english = makeContent(self.folder, 'SimpleType', 'doc')
+
+    def test_getTypeInfo(self):
+        """
+        LinguaPlone uses TypeInfoWrapper to change 'edit' action to
+        'translate_item' on non translations.
+        """
+
+        ti = self.english.getTypeInfo()
+        self.assertTrue(isinstance(ti, TypeInfoWrapper))
+
+    def test_getTypeInfo_nosite(self):
+        """
+        getTypeInfo should not break when site has not been set
+        """
+        from zope.site.hooks import getSite
+        from zope.site.hooks import setSite
+
+        # save site before unhooking it in order to set it back after the test
+        saved_site = getSite()
+
+        setSite(None)
+
+        ti = self.english.getTypeInfo()
+
+        self.assertTrue(isinstance(ti, TypeInfoWrapper))
+
+        # reset site to avoid breaking other tests
+        setSite(saved_site)